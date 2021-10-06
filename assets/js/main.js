f307c1521dfb747988c5ef5400d82d6b1b19d49d
Ardackm <79649337+Ardackm@users.noreply.github.com>
2021-10-06 20:32:08 +0300

    web-site-desing

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// navbar
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
+
+// disable right click
+
+window.addEventListener('contextmenu', function (e) { 
+  // do something here... 
+  e.preventDefault(); 
+}, false);
+
+
